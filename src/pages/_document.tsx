cde83eacd776af52c3aefd19ac1ab7eed8bb6b25
João Victor <jv.joaovictor.bezerra.42@gmail.com>
2021-09-25 23:45:17 -0300

    fix: stylesheet import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <title>Marque Saúde</title>
           <meta name="description" content="Marque Saúde app"/>
           <link rel="icon" href="/favicon.ico" />
-          <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"/>
         </Head>
