57f688fcb5445fd0e3c1e27049b68af072112dad
João Victor <jv.joaovictor.bezerra.42@gmail.com>
2022-01-24 17:00:42 -0300

    chore: change database to planetscale

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,7 @@
 // @ts-nocheck
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
+const prisma = global.prisma || new PrismaClient()
+if (process.env.NODE_ENV === 'development') global.prisma = prisma
 
 export default prisma
